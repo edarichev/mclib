3dcfd0641f7bd602eede18ae4f95fc60828f37d5
edarichev <edarichev@gmail.com>
2024-03-31 16:15:31 +0300

    Add examples project

---
+++ network/ESP01.h
@@ -0,0 +1,30 @@
+/**
+ * ESP-01
+ * 
+ * @author Evgeny Darichev
+ * @date 30.03.2024
+ */
+
+#ifndef _ESP01_H_INCLUDED_
+#define _ESP01_H_INCLUDED_
+
+#include <cstring>
+#include <memory>
+#include "../xuart.h"
+
+class ESP01 : public UARTClient
+{
+public:
+protected:
+	void send(const char *data, size_t len = -1)
+	{
+		if (!data)
+			return;
+		if (len < 0)
+			len = strlen(data);
+		if (!len)
+			return;
+	}
+};
+
+#endif // _ESP01_H_INCLUDED_
--- xuart.h
+++ xuart.h
@@ -4,19 +4,23 @@
 #include <cstdio>
 #include <cstring>
 #include <cstdarg>
+#include <memory>
+#include <stdlib.h>
 
 #if defined(PLATFORM_STM32_HAL)
 
 #if defined(HAL_UART_MODULE_ENABLED)
-class XUART
+
+class UARTClient
 {
+private:
 	UART_HandleTypeDef *_huart = nullptr;
 public:
-	XUART(UART_HandleTypeDef *huart) : _huart(huart)
-	{
+	UARTClient(UART_HandleTypeDef *huart) : _huart(huart) {}
 
-	}
-
+	/**
+	 * Writes the string to UART. Uses buffer on the stack with 80 chars.
+	 */
 	void write(const char *format, ...)
 	{
 		char msg[80];
@@ -27,6 +31,26 @@ public:
 		_write(msg);
 	}
 
+	/**
+	 * Writes the string to UART. Uses dinamycally allocated buffer with specified size.
+	 */
+	void write(size_t bufAllocSize, const char *format, ...)
+	{
+		if (!bufAllocSize)
+			return;
+		char *msg = new char [bufAllocSize];
+		va_list args;
+		va_start(args, format);
+		vsprintf(msg, format, args);
+		va_end(args);
+		_write(msg);
+		delete [] msg;
+	}
+
+	/**
+	 * Writes the string with \r\n ending to UART.
+	 * Uses buffer on the stack with 80 chars.
+	 */
 	void writeLine(const char *format, ...)
 	{
 		char msg[80];
@@ -36,7 +60,20 @@ public:
 		va_end(args);
 		_writeLine(msg);
 	}
-private:
+
+	void writeLine(size_t bufAllocSize, const char *format, ...)
+	{
+		if (!bufAllocSize)
+			return;
+		char *msg = new char [bufAllocSize];
+		va_list args;
+		va_start(args, format);
+		vsprintf(msg, format, args);
+		va_end(args);
+		_writeLine(msg);
+		delete [] msg;
+	}
+protected:
 	void _write(const char *msg)
 	{
 		HAL_UART_Transmit(_huart, (uint8_t*) msg, strlen(msg), HAL_MAX_DELAY);
@@ -49,6 +86,24 @@ private:
 		HAL_UART_Transmit(_huart, (uint8_t*) EOL, strlen(EOL), HAL_MAX_DELAY);
 	}
 };
+
+class UARTLogger : public UARTClient
+{
+public:
+	UARTLogger(UART_HandleTypeDef *huart) : UARTClient(huart)
+	{
+
+	}
+
+	// TODO: create debug & release version
+	void xassert(bool condition, const char *errorMessage)
+	{
+		if (condition)
+			return;
+		_writeLine(errorMessage);
+	}
+};
+
 #endif // defined(HAL_UART_MODULE_ENABLED)
 
 #else
